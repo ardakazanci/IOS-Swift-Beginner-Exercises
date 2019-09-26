549ff03c390a13b1d19a77ad11c0587273cf751c
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-09-26 20:21:01 +0300

    Layout Exercises - Day 1
    Layout Exercises - Day 1

---
+++ LayoutExercises/LayoutExercises/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutExercises
+//
+//  Created by Arda Kazancı on 26.09.2019.
+//  Copyright © 2019 Arda Kazancı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
