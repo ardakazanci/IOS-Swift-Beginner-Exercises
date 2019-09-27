f2c76f02579d58319de23c65d0d661808e08778e
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-09-27 22:13:05 +0300

    Day 3 - Programmatically Button Added
    Day 3 - Programmatically Button Added

--- NotStoryBoardExercises/NotStoryBoardExercises/ViewController.swift
+++ NotStoryBoardExercises/NotStoryBoardExercises/ViewController.swift
@@ -12,25 +12,58 @@ class ViewController: UIViewController {
     
     
     let myLabel  = UILabel()
+    let myButton = UIButton()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+            
+        
+       
+        
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        myButton.setTitle("My Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.brown, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: 0.5 - 100, y: height * 0.6, width: 200, height: 100)
+               
+        
         
         myLabel.text = "My Label"
         myLabel.textAlignment = .center
         
         // EKRANDA GÖSTERİLMESİ İÇİN
-        myLabel.frame = CGRect.init(x: 100, y: 100, width: 100, height: 100)
+        myLabel.frame = CGRect.init(x: width / 2 * width * 0.8 / 2, y: height * 0.5 - 50 / 2, width: width * 0.8, height: 50)
         // Oluşturduğumuz label ' ı sürükle bırak yaptığımız gibi view ' a atıyoruz.
         view.addSubview(myLabel)
+        view.addSubview(myButton)
         
         // Tüm ekrana uyumlu olması için. Ekranın width ve height değerini alarak buna göre işlem yapabilmemiz gerek.
+        /**
+                 
+         self -> ViewController ' ın kendisi.
+         
+         */
         
         
+        /*
+         self : Butona tıklandığında nereden aksiyon çağıracağız ın cevabıdır ViewController - > Self içinde bulunduğu sınıf.
+         action : Çağırılacak aksiyon hangisi ? ViewController sınıfı içerisinde yer alan method.
+         for : Action nasıl çağırılacak ın cevabıdır For kısmı. Meseal tıklandığında touchUpInside
+         */
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControl.Event.touchUpInside)
     }
 
+    
+   @objc func myAction()
+    {
+    
+        print("Function")
+        
+    }
 
 }
 
