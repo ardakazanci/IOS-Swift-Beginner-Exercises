51196683b204b4b0907eb5c12950486bc59ae315
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-09-29 19:38:35 +0300

    Segue - Part 1
    Segue - Part 1

---
+++ BirdenFazlaEkranIleCalismak/BirdenFazlaEkranIleCalismak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirdenFazlaEkranIleCalismak
+//
+//  Created by Arda Kazancı on 29.09.2019.
+//  Copyright © 2019 Arda Kazancı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BirdenFazlaEkranIleCalismak/BirdenFazlaEkranIleCalismak/ViewControllerScreen2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerScreen2.swift
+//  BirdenFazlaEkranIleCalismak
+//
+//  Created by Arda Kazancı on 29.09.2019.
+//  Copyright © 2019 Arda Kazancı. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerScreen2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
