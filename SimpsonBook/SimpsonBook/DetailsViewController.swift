d5f8c9a9d0bf938541a8301f26a72fc42c5a5249
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-10-23 20:34:07 +0300

    Day 7 -Simpson Book Simple Exercise
    Day 7 -Simpson Book Simple Exercise

---
+++ SimpsonBook/SimpsonBook/DetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailsViewController.swift
+//  SimpsonBook
+//
+//  Created by Arda Kazancı on 23.10.2019.
+//  Copyright © 2019 Arda Kazancı. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var detailsImageView: UIImageView!
+    
+    @IBOutlet weak var detailsName: UILabel!
+    
+    @IBOutlet weak var detailsJob: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SimpsonBook/SimpsonBook/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Arda Kazancı on 23.10.2019.
+//  Copyright © 2019 Arda Kazancı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
