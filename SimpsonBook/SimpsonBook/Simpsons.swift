1499bfc79e51a3fe475ae36a28f2779b45e071ff
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-10-24 21:00:10 +0300

    Day 9 - IOS Swift Exercise
    Day 9 - IOS Swift Exercise

---
+++ SimpsonBook/SimpsonBook/Simpsons.swift
@@ -0,0 +1,28 @@
+//
+//  Simpsons.swift
+//  SimpsonBook
+//
+//  Created by Arda Kazancı on 24.10.2019.
+//  Copyright © 2019 Arda Kazancı. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Simpsons {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName : String, simpsonJob:String,simpsonImage:UIImage) {
+        
+        self.name = simpsonName
+        self.job = simpsonJob
+        self.image = simpsonImage
+        
+    }
+    
+    
+}
--- SimpsonBook/SimpsonBook/ViewController.swift
+++ SimpsonBook/SimpsonBook/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
+    
+    
+   
+    
 
     @IBOutlet weak var mainTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
+        
+               let homer = Simpsons(simpsonName: "Homer", simpsonJob: "Nuclear Safety", simpsonImage: UIImage(named: "homer")!)
+        
+               let marge = Simpsons(simpsonName: "Marge", simpsonJob: "HouseWife", simpsonImage: UIImage(named: "marge")!)
+        
+               let bart = Simpsons(simpsonName: "Bart", simpsonJob: "Student", simpsonImage: UIImage(named: "bart")!)
+                
+               let lisa = Simpsons(simpsonName: "Lisa", simpsonJob: "Student", simpsonImage: UIImage(named: "lisa")!)
+        
+        let homerArray = [homer,marge,bart,lisa]
+        
+    }
+    
+    
+    // Kaç tane row olacak
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 10
+    }
+    
+    
+    // TableView içerisinde nasıl bir gösterim olacak
+    // Her bir hücre içerisinde gösterilecek değerler.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell() // Hücre görünümü.
+        cell.textLabel?.text = "Sample Simpsons"
+        return cell
+        
+        
     }
 
 
